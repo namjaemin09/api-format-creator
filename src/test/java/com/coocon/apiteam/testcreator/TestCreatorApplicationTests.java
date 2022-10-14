570048b295e4e59f7daea1954afed279e3b2b353
LAPTOP-SL0JNDB7\NAMJAEMIN <namjaemin09@coocon.net>
2022-10-14 17:14:00 +0900

    init

---
+++ src/test/java/com/coocon/apiteam/testcreator/TestCreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coocon.apiteam.testcreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCreatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
